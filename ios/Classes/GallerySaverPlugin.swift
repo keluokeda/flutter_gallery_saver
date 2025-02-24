12b0b4a9f3b2a2846754d78863c4c1e7d470a516
keluokeda <913918146@qq.com>
2025-02-24 11:50:14 +0800

    格式化代码

--- ios/Classes/GallerySaverPlugin.swift
+++ ios/Classes/GallerySaverPlugin.swift
@@ -2,34 +2,34 @@ import Flutter
 import UIKit
 
 public class GallerySaverPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "gallery_saver", binaryMessenger: registrar.messenger())
-    let instance = GallerySaverPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    case "toGallery":
-      toGallery(call: call, result: result)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "gallery_saver", binaryMessenger: registrar.messenger())
+        let instance = GallerySaverPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
-    
-    private func toGallery(call: FlutterMethodCall, result: @escaping FlutterResult){
-      let map = call.arguments as! Dictionary<String,String>
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "toGallery":
+            toGallery(call: call, result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func toGallery(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let map = call.arguments as! [String: String]
 //        print(map)
         let url = map["path"]
         if let image = UIImage(contentsOfFile: url!) {
-                       UIImageWriteToSavedPhotosAlbum(image, self, #selector(didFinishSavingImage(image:error:contextInfo:)), nil)
-                   }
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(didFinishSavingImage(image:error:contextInfo:)), nil)
+        }
         result(nil)
     }
-    
+
     @objc func didFinishSavingImage(image: UIImage, error: NSError?, contextInfo: UnsafeMutableRawPointer?) {
 //           saveResult(isSuccess: error == nil, error: error?.description)
-       }
+    }
 }
